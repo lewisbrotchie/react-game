c28a16197bec3b01055b9d8a18ce27b8f4f948aa
lewisbrotchie <noble.ninja@gmail.com>
2018-05-21 16:18:46 +0100

    integrated redux and started svg canvas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
+import Canvas from "./components/Canvas";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Test</h1>
-      </div>
-    );
+    return <Canvas />;
   }
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: `Integrated React and Redux`
+};
+
+function reducer(state = initialState) {
+  return state;
+}
+
+export default reducer;
