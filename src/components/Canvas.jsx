b09e1d017d071fecf982464997f7e762b7bf3ed7
lewisbrotchie <noble.ninja@gmail.com>
2018-05-23 20:41:41 +0100

    implemented flying objects appearing at random positions

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -45,8 +45,12 @@ const Canvas = props => {
       )}
       {props.gameState.started && (
         <g>
-          <FlyingObject position={{ x: -150, y: -300 }} />
-          <FlyingObject position={{ x: 150, y: -300 }} />
+          {props.gameState.flyingObjects.map(flyingObject => (
+            <FlyingObject
+              key={flyingObject.id}
+              position={flyingObject.position}
+            />
+          ))}
         </g>
       )}
     </svg>
@@ -58,10 +62,19 @@ Canvas.propTypes = {
   gameState: PropTypes.shape({
     started: PropTypes.bool.isRequired,
     kills: PropTypes.number.isRequired,
-    lives: PropTypes.number.isRequired
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(
+      PropTypes.shape({
+        position: PropTypes.shape({
+          x: PropTypes.number.isRequired,
+          y: PropTypes.number.isRequired
+        }).isRequired,
+        id: PropTypes.number.isRequired
+      })
+    ).isRequired
   }).isRequired,
   trackMouse: PropTypes.func.isRequired,
-  StartGame: PropTypes.func.isRequired
+  startGame: PropTypes.func.isRequired
 };
 
 export default Canvas;
